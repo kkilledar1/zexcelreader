d029661e4d73cad876b5b40b7fad4604ed584585
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-13 15:36:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'Classes/PHPexcel.php';
+include 'PHPExcel.php'
 
 $tmpfname="C:\Users\killekb\Documents\work related\Excelread\test.xlsx";
 $excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
