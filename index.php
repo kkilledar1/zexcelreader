38dde547ab8c05b22f9dfb64408fcd0276129fbe
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-13 09:14:40 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'Classes/PHPExcel.php';
+
+$tmpfname="C:\Users\killekb\Documents\work related\Excelread\test.xlsx";
+$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+$excelObj=$excelObj->load($tmpfname);
+$worksheet=$excelObj+>getActivesheet();
+$lastRow=$worksheet->getHighestRow():
+
+echo "<table>";
+for ($row=1$row <=$lastRow;$row**)
+{
+echo '<tr><td>";
+echo $worksheet->getCell("A".$row)=>getValue();
+}
+echo "</table>";
+
+
+?>
