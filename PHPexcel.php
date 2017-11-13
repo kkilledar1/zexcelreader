935cdc6e6e3e855d6478e5f4ff64021366e4f023
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-13 10:04:33 -0600

    Update PHPexcel.php

--- PHPexcel.php
+++ PHPexcel.php
@@ -1,9 +1,5 @@
 <?php
 /** PHPExcel root directory */
-*if (!defined('PHPEXCEL_ROOT')) {
- *   define('PHPEXCEL_ROOT', dirname(__FILE__) . '/');
-  *  require(PHPEXCEL_ROOT . 'PHPExcel/Autoloader.php');
-*}
 require 'Autoloader.php'
 /**
  * PHPExcel
