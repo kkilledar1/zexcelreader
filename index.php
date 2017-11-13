c0c9b73efb71591afed49c72c51ad0e4a84b4e19
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-13 15:41:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 include 'PHPExcel.php'
 
-$tmpfname="C:\Users\killekb\Documents\work related\Excelread\test.xlsx";
-$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+$inputFileName="C:\Users\killekb\Documents\work related\Excelread\test.xlsx";
+$excelReader = PHPExcel_IOFactory::createReaderForFile($inputFileName);
 $excelObj=$excelObj->load($tmpfname);
 $worksheet=$excelObj->getActivesheet();
 $lastRow=$worksheet->getHighestRow();
