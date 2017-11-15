4c5731c64d1c73c3e8817ff10a52feaf199e0d18
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-15 13:15:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 /** Include PHPExcel */
-//require_once dirname(__FILE__) . '/../Classes/PHPExcel.php';
+require_once dirname(__FILE__) . 'zexcelreader/Classes/PHPExcel.php';
 // Create new PHPExcel object
-require_once zexcelreader/Classes/PHPExcel.php;
 echo 'Hello World';
 ?>
