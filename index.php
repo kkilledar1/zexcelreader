fc3a202464f4a36884b8f4f1988977e7e0cfdb57
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-16 17:47:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /** Include PHPExcel */
-require_once dirname(__FILE__) . '/../Classes/PHPExcel.php';
+require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
 //include "Classes/PHPExcel.php";
 //require_once ('PHPExcel.php');
 // Create new PHPExcel object
