5066a267781438a4fd8f3fd99b4ac717e98156e6
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-17 10:05:14 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,32 @@ require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
 //require_once ('PHPExcel.php');
 // Create new PHPExcel object
 echo 'Hello World';
+
+ include('/Classes/PHPExcel/IOFactory.php');
+
+  //Use whatever path to an Excel file you need.
+  $inputFileName = 'C:\Users\killekb\Documents\work related\Excelread\test.xlsx';
+
+  try {
+    $inputFileType = PHPExcel_IOFactory::identify($inputFileName);
+    $objReader = PHPExcel_IOFactory::createReader($inputFileType);
+    $objPHPExcel = $objReader->load($inputFileName);
+  } catch (Exception $e) {
+    die('Error loading file "' . pathinfo($inputFileName, PATHINFO_BASENAME) . '": ' . 
+        $e->getMessage());
+  }
+
+  $sheet = $objPHPExcel->getSheet(0);
+  $highestRow = $sheet->getHighestRow();
+  $highestColumn = $sheet->getHighestColumn();
+
+  for ($row = 1; $row <= $highestRow; $row++) { 
+    $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, 
+                                    null, true, false);
+
+    //Prints out data in each row.
+    //Replace this with whatever you want to do with the data.
+    echo '<pre>';
+      print_r($rowData);
+    echo '</pre>';
 ?>
