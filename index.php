2d140ab95e6dd8aeb5ea3ff41e0c0ab06130f497
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-18 09:39:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
 //require_once ('PHPExcel.php');
 // Create new PHPExcel object
 echo 'Hello World';
-$searchValue = 'B';
+$searchValue = 'A';
 
  $tmpfname = "test.xls";
 		$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
@@ -15,11 +15,11 @@ $searchValue = 'B';
 		
 echo "<table>";
 		for ($row = 1; $row <= $lastRow; $row++) {
-			 echo "<tr><td>";
-			 echo $worksheet->getCell('A'.$row)->getValue();
-			 echo "</td><td>";
-			 echo $worksheet->getCell('B'.$row)->getValue();
-			 echo "</td><tr>";
+			 //echo "<tr><td>";
+			 //echo $worksheet->getCell('A'.$row)->getValue();
+			 //echo "</td><td>";
+			 //echo $worksheet->getCell('B'.$row)->getValue();
+			 //echo "</td><tr>";
 		
 			if ($worksheet->getCell('A'.$row)->getValue() == $searchValue)	{
                         echo "Found it";
@@ -30,6 +30,6 @@ echo "<table>";
 		}
 
 echo "</table>";
-		echo 'Excel read';
+		//echo 'Excel read';
 
 ?>
