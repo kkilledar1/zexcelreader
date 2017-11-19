6f14a91843f5bc2243bed708a89e5fdde019752b
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-18 21:46:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
 //require_once ('PHPExcel.php');
 // Create new PHPExcel object
 //echo 'Hello World';
-$searchValue = 'B';
+$searchValue = 'C';
 
  $tmpfname = "test.xls";
 		$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
@@ -14,7 +14,7 @@ $searchValue = 'B';
 		$lastRow = $worksheet->getHighestRow();
 		
 echo "<table>";
-		for ($row = 2; $row <= $lastRow; $row++) {
+		for ($row = 1; $row <= $lastRow; $row++) {
 			 //echo "<tr><td>";
 			 //echo $worksheet->getCell('A'.$row)->getValue();
 			 //echo "</td><td>";
