ff46abe03789238d11ea0655062717c808705983
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-11-19 08:46:45 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $method = $_SERVER['REQUEST_METHOD'];
  
 // Process only when method is POST
-if($method == 'POST'){
+//if($method == 'POST'){
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
  
@@ -51,10 +51,10 @@ if (empty($column)){
     $response->displayText = $column;
     $response->source = "webhook";
     echo json_encode($response);
-}
-else
-{
-    echo "Method not allowed";
-}
+//}
+//else
+//{
+  //  echo "Method not allowed";
+//}
  
 ?>
